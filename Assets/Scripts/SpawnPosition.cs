33d0f7af4953e4b986c98ba754392b93d331ee42
leolaz99 <leolaz99@gmail.com>
2020-12-21 15:10:48 +0100

    add 4 direction of spawn

---
+++ Assets/Scripts/SpawnPosition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+    [SerializeField] GameObject spawn1;
+    [SerializeField] GameObject spawn2;
+    [SerializeField] GameObject spawn3;
+    [SerializeField] GameObject spawn4;
+    float timer = 0;
+
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= 1)
+        {
+            int rand = Random.Range(1, 5);
+            if (rand == 1)
+                spawn1.SetActive(true);
+            
+            if (rand == 2)
+                spawn2.SetActive(true);
+            
+            if (rand == 3)
+                spawn3.SetActive(true);
+           
+            if (rand == 4)
+                spawn4.SetActive(true);
+
+            timer = 0;
+        }
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,19 +2,14 @@
 
 public class Spawner : MonoBehaviour
 {
-    float timer = 0;
     [SerializeField] GameObject prefab;
     [SerializeField] Transform spawn1;
     [SerializeField] Transform spawn2;
 
     void Update()
     {
-        timer = timer + Time.deltaTime;
-        if (timer >= 1)
-        {
-            float rand = Random.Range(spawn1.position.z, spawn2.position.z);
-            Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, rand), transform.rotation);
-            timer = 0;
-        }
+        float rand = Random.Range(spawn1.position.z, spawn2.position.z);
+        Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, rand), transform.rotation);
+        gameObject.SetActive(false);
     }
 }
