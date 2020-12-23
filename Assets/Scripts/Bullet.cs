3ae32bae0ef68f6173d3608759a3d4cef5fb61f3
leolaz99 <leolaz99@gmail.com>
2020-12-23 16:01:33 +0100

    add test homing shot

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,8 +2,16 @@
 
 public class Bullet : MonoBehaviour
 {
+    GameObject target;
+
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+    
     void Update()
     {
         transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+        transform.LookAt(target.transform);
     }
 }
